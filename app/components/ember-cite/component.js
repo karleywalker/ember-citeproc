ff611536a90c3a9a9c122b97675a1e418a05df52
Karley Walker <kbw2as@virginia.edu>
2017-06-14 15:52:53 -0400

    First Commit

---
+++ app/components/ember-cite/component.js
@@ -0,0 +1,106 @@
+import Ember from 'ember';
+/* global citeproc */
+
+export default Ember.Component.extend({
+
+  didInsertElement(){
+    this._super(...arguments);
+    var data = this ;
+    var newCite = this.$('.json-data').html();
+    var citationData = JSON.parse(newCite);
+    var chosenStyleID = "chicago-fullnote-bibliography";
+    var citations = {};
+    var itemIDs = [];
+
+    for (var i=0,ilen=citationData.items.length;i<ilen;i++) {
+      var item = citationData.items[i];
+      if (!item.issued) continue;
+      if (item.URL) delete item.URL;
+      var id = item.id;
+      citations[id] = item;
+      itemIDs.push(id);
+    }
+
+    var citeprocSys = {
+        retrieveLocale: function (lang){
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', 'https://raw.githubusercontent.com/Juris-M/citeproc-js-docs/master/locales-' + lang + '.xml', false);
+          xhr.send(null);
+          return xhr.responseText;
+        },
+        retrieveItem: function(id){
+          return citations[id];
+        }
+      };
+
+      function getProcessor(styleID) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://raw.githubusercontent.com/citation-style-language/styles/master/' + styleID + '.csl', false);
+        xhr.send(null);
+        var styleAsText = xhr.responseText;
+        var citeproc = new CSL.Engine(citeprocSys, styleAsText);
+        return citeproc;
+      };
+
+      function processorOutput() {
+        var citeproc = getProcessor(chosenStyleID);
+        citeproc.updateItems(itemIDs);
+        var bibResult = citeproc.makeBibliography();
+        return bibResult[1].join('\n');
+      };
+
+      var content = document.getElementById("csl-content-block");
+      content.innerHTML = processorOutput();
+
+  },
+
+  actions: {
+      updatesCitations: function() {
+        var newCite = document.getElementById("json-text").value;
+        var citationData = JSON.parse(newCite);
+        var chosenStyleID = "chicago-fullnote-bibliography";
+        var citations = {};
+        var itemIDs = [];
+
+        for (var i=0,ilen=citationData.items.length;i<ilen;i++) {
+          var item = citationData.items[i];
+          if (!item.issued) continue;
+          if (item.URL) delete item.URL;
+          var id = item.id;
+          citations[id] = item;
+          itemIDs.push(id);
+        }
+
+        var citeprocSys = {
+          retrieveLocale: function (lang){
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://raw.githubusercontent.com/Juris-M/citeproc-js-docs/master/locales-' + lang + '.xml', false);
+            xhr.send(null);
+            return xhr.responseText;
+          },
+          retrieveItem: function(id){
+            return citations[id];
+          }
+        };
+
+        function getProcessor(styleID) {
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', 'https://raw.githubusercontent.com/citation-style-language/styles/master/' + styleID + '.csl', false);
+          xhr.send(null);
+          var styleAsText = xhr.responseText;
+          var citeproc = new CSL.Engine(citeprocSys, styleAsText);
+          return citeproc;
+        };
+
+        function processorOutput() {
+          var citeproc = getProcessor(chosenStyleID);
+          citeproc.updateItems(itemIDs);
+          var bibResult = citeproc.makeBibliography();
+          return bibResult[1].join('\n');
+        };
+
+        var contentblock = document.getElementById("csl-content-block");
+        contentblock.innerHTML = processorOutput();
+      }
+  }
+});
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -9,6 +9,9 @@ module.exports = function(defaults) {
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
+
+  app.import('vendor/citeproc.js');
+
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
   // object's keys should be the environment name and the values
